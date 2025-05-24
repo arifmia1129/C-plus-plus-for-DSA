2397128b923320de3e657793a73601ac6c8ce853
Md. Arif <arifmia1129@gmail.com>
2025-05-24 22:20:20 +0600

    Print in C++

---
+++ print.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main() {
+    int num = 10;
+    char c = 'B';
+
+    std::cout << "My favorite number is: " << num << std::endl << "My favorite character is: " << c;
+
+    return 0;
+}
