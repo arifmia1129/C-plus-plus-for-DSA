b4bd6dc41de21c4bfb5adbe03e2a10cc21196508
Md. Arif <arifmia1129@gmail.com>
2025-05-24 22:32:31 +0600

    Input output and typecast

---
+++ input_output_typecast.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int x;
+    char c;
+
+    cin >> x >> c;
+
+    cout << "Output: " << x << " " << c << endl;
+    cout << "ascii: " << (char)x << " " << (int) c;
+}
