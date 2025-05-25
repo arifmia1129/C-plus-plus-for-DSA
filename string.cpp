8539a2fa97c866ce0e348a47ab79188e8a8b2e7b
Md. Arif <arifmia1129@gmail.com>
2025-05-25 07:07:40 +0600

    String

---
+++ string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int n;
+    char s[10001];
+    string name;
+
+    cin >> n;
+
+    cin.ignore();
+
+    // cin >> s;
+    cin.getline(s, 10001);
+
+    cin >> name;
+
+    cout << n << endl << s << endl << name;
+
+}
