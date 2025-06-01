5672e15404e98813b3a7105ac6d13ee5a0fd566e
Md. Arif <arifmia1129@gmail.com>
2025-06-01 19:52:29 +0600

    String data type

---
+++ string_cpp.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    string s1 = "Assalamu";
+    string s2 = "Alaikum";
+
+    if(1 == 1) {
+        cout << s1 << " " << s2 << endl;
+    }
+
+    return 0;
+}
