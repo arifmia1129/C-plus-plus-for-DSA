dfc70a6a5487690519f88c7931d22b730ecdc810
Md. Arif <arifmia1129@gmail.com>
2025-06-02 06:18:59 +0600

    String modifiers

--- string_modifiers.cpp
+++ string_modifiers.cpp
@@ -8,13 +8,28 @@ int main () {
     // s1 += s2;
     s1.append(s2);
 
-    s1.push_back('A');
+    // s1.push_back('A');
 
-    s1 += "Okay";
+    // s1 += "Okay";
 
-    s1[0] = 'G';
+    // s1[0] = 'G';
 
-    s1.pop_back();
+    // s1.pop_back();
+
+    // s1 = "Gello";
+
+    s1.assign("Gello ");
+    s1 += s2;
+
+    // s1.erase(5, 1);
+
+    s1[0] = 'H';
+
+
+    s1.replace(6, 5, "Bangladesh");
+
+    s1.insert(6, "Arif ");
+
 
     cout << s1;
 
