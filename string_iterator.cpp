d7d3acb1268ce279d84aa23e702d352a819b4d23
Md. Arif <arifmia1129@gmail.com>
2025-06-02 06:30:33 +0600

    String iterator

---
+++ string_iterator.cpp
@@ -0,0 +1,25 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main () {
+//     string str = "Hello World";
+
+//     for(string::iterator it = str.begin(); it < str.end(); it++) {
+//         cout << *it << " ";
+//     }
+
+//     return 0;
+// }
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    string s = "Asslamualaikum";
+
+    for(auto it = s.begin(); it < s.end(); it++) {
+        cout << *it << " ";
+    }
+
+    return 0;
+}
