2650cc8e86f258835d63600e3e067c3434385dc4
Md. Arif <arifmia1129@gmail.com>
2025-06-02 07:09:31 +0600

    String Constructor

---
+++ string_constructor.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+    // string s("Hello World");
+
+    // string s("Hello World", 4);
+
+    // string t(s, 4);
+
+    string s(10, 'A');
+
+    cout << s << endl;
+
+    return 0;
+}
