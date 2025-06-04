716016c24a5a92c5c6344b13941699cf504d73ba
Md. Arif <arifmia1129@gmail.com>
2025-06-04 08:19:46 +0600

    problem solved

--- sort_student_object.cpp
+++ sort_student_object.cpp
@@ -5,28 +5,15 @@ class Student {
     public:
         string name;
         int roll;
-        int marks;
+        int marks;  
 };
 
 bool cmp(Student l, Student r) {
-    // if(l.marks > r.marks) {
-    //      return true;
-    // }else if(l.marks < r.marks) {
-    //     return false;
-    // }else{
-    //     if(l.roll < r.roll) {
-    //         return true;
-    //     }else{
-    //         return false;
-    //     }
-    // }
-
-    return l.marks == r.marks ? l.roll < r.roll : l.marks > r.marks;
-
-    // return l.marks > r.marks;
+    return l.marks == r.marks ? l.roll > r.roll : l.marks < r.marks;
 }
 
 int main () {
+
     int n;
 
     cin >> n;
@@ -39,9 +26,10 @@ int main () {
 
     sort(a, a + n, cmp);
 
-    for(int i = 0; i < n; i++) {
+     for(int i = 0; i < n; i++) {
         cout << " " << a[i].name << " " << a[i].roll << " " << a[i].marks << endl;
     }
+
     
     return 0;
 }
